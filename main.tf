79a8882bfb7e473780baa52776bab6419bc23a15
John Farnell <46055195+farnejb@users.noreply.github.com>
2021-10-19 15:32:10 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,8 @@ resource "local_file" "foo" {
     content     = "foo!"
     filename = "/home/ubuntu/foo.bar"
 }
+
+resource "local_file" "foo-1" {
+    content     = "foo!"
+    filename = "/home/ubuntu/foo.bar"
+}
