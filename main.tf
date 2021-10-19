e7f7a363c98da70c2c5f520239dfbb2d23f4eb81
John Farnell <46055195+farnejb@users.noreply.github.com>
2021-10-19 13:54:11 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,8 @@
 terraform {
-  required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
-      version = "2.13.0"
-    }
-  }
   required_version = ">= 0.13"
 }
 
-provider "docker" {
-  # host    = "npipe:////.//pipe//docker_engine"
-  host = "tcp://127.0.0.1:2375/"
-}
-
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-}
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "nginx"
-  ports {
-    internal = 80
-    external = 8000
-  }
+resource "local_file" "foo" {
+    content     = "foo!"
+    filename = "~/foo.bar"
 }
